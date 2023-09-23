7be9a78d87581d9e549f01b34d714d2fb5d12d3e
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-09-23 22:42:04 +0530

    completed star task

--- StarAlgo1.java
+++ StarAlgo1.java
@@ -1,5 +1,10 @@
 public class StarAlgo1 {
     public static void main(String[] args) {
-        System.out.println("*");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
